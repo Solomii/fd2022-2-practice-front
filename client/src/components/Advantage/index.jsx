1838819b3390abe00b2de8246a01db41555d5a83
Solomiia Kushniryk <kushniryksolomiya@gmail.com>
2023-03-29 16:18:48 +0300

    Add style and advantage.json

--- client/src/components/Advantage/index.jsx
+++ client/src/components/Advantage/index.jsx
@@ -1,56 +1,22 @@
 import React from 'react';
-import CONSTANTS from '../../constants'
-import styles from "./Advantage.module.scss"
+import CONSTANTS from '../../constants';
+import styles from './Advantage.module.sass';
+import advantages from './advantage.json';
 
 const Advantage = () => {
-  return (
-    <div className={styles.container__description}>
-    <h2 className={styles.blueUnderline}>Why Squadhelp?</h2>
-    <div className={styles.cardContainer}>
-      <div className={styles.card}>
-        <img
-          src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-world-icon.png`}
-          alt='globe'
-        />
-        <h3>Largest Naming Community</h3>
-        <p>
-          Our unique approach allows you to receive an unmatched
-          breadth of business name ideas from world's largest
-          community of naming experts. With 75,000+ creatives and
-          15,000+ successful naming projects, Squadhelp is by far the
-          largest naming platform across the globe .
-        </p>
-      </div>
-      <div className={styles.card}>
-        <img
-          src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-high-quality-icon.png`}
-          alt='desktop'
-        />
-        <h3>High Quality & Collaboration</h3>
-        <p>
-          Using an advanced Quality Scoring Algorithm and Machine
-          Learning, we ensure that you receive more ideas from our
-          top-quality creatives, and Gamification best practices
-          ensure two-way communication throughout your contest.
-        </p>
-      </div>
-      <div className={styles.card}>
-        <img
-          src={`${CONSTANTS.STATIC_IMAGES_PATH}more-benifits-trademark-icon.png`}
-          alt='cards'
-        />
-        <h3>Agency-Level Features</h3>
-        <p>
-          Squadhelp's high end Audience Testing service allows you to
-          poll your target demographics to get unbiased feedback on
-          your favorite names. Also receive Trademark support from our
-          team of Licensed Trademark Attorneys, so you can pick your
-          name with confidence.
-        </p>
-      </div>
-    </div>
-  </div>
+  const renderCard = ({ imgPath, imgAlt, title, content }, i) => (
+    <article key={i} className={styles.card}>
+      <img alt={imgAlt} src={`${CONSTANTS.STATIC_IMAGES_PATH}${imgPath}`} />
+      <h3>{title}</h3>
+      <p>{content}</p>
+    </article>
   );
-}
+  return (
+    <section className={styles.container__description}>
+      <h2 className={styles.blueUnderline}>Why Squadhelp?</h2>
+      <div className={styles.cardContainer}>{advantages.map(renderCard)}</div>
+    </section>
+  );
+};
 
- export default  Advantage;
+export default Advantage;
